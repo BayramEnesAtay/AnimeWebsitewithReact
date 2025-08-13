68c5b963bf3c3fca7061c627515dbcd2e6982130
BayramEnesAtay <bayrameness4242@gmail.com>
2025-08-13 18:20:45 +0300

    Replace Components with components

--- src/Components/footer/Style.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export  const Pagination=styled.div`
-    display: flex;
-    flex-direction: row;
-    column-gap: 5px;
-    justify-content: center;
-    align-items: center;
-  `
