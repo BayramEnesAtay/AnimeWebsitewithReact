7b00edab210f2661fec68d302667e4ff4455a7d8
BayramEnesAtay <bayrameness4242@gmail.com>
2025-09-26 10:44:24 +0300

    Feat:Finish the Sign In page design.

--- src/components/singIn/index.jsx
+++ src/components/singIn/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import {Section,GeneralSection,Title,Logo,Text,Motto,LogIn,Email,Label,Input,Mail_Icon,Icon}from "./styled";
-import { CiMail } from "react-icons/ci";
+import {Section,GeneralSection,Title,Logo,Text,Motto,LogIn,Email,Label,Input,Mail_Icon,Icon,LockIcon,Button,OrSection,Pargfh,Google,FaceBook,Buttons, InnerDiv, P2,P1,Footer,Img}from "./styled";
+import { FaLock } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import SignInImage  from "../../images/SignInImage.jpg"
 
 const SignIn=()=>{
   return(
+    <>
+  <Img src={SignInImage}/>
   <GeneralSection>
+    
     <Section>
       <Title>
         <Logo>A</Logo>
@@ -18,11 +24,32 @@ const SignIn=()=>{
             <Icon />
             <Input placeholder="Enter your email" />
           </Mail_Icon>
-          
         </Email>
+        <Email>
+          <Label>Password</Label>
+          <Mail_Icon>
+            <LockIcon/>
+            <Input type="password"  placeholder="Enter your password" />
+          </Mail_Icon>
+        </Email>
+        <Button>
+          Sign In
+        </Button>
+        <OrSection>
+          <Pargfh>OR CONTINUE WITH</Pargfh>
+        </OrSection>
+        <Buttons>
+          <Google><InnerDiv><FaGoogle />Google</InnerDiv></Google>
+          <FaceBook><InnerDiv><FaFacebook />Facebook</InnerDiv></FaceBook>
+        </Buttons>
+        <Footer>
+          <P1>Don&apos;t have an account?</P1>
+          <P2>Sign up</P2>
+        </Footer>
       </LogIn>
     </Section>
   </GeneralSection>
+  </>
   );
   
 }
--- src/components/singIn/styled.js
+++ src/components/singIn/styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { GenreButtons } from "../searchandfilterbar/styled";
-import { FaEnvelope } from "react-icons/fa";
+import { FaEnvelope,FaLock } from "react-icons/fa";
+
 
 export  const Section=styled.div`
   display:flex;
@@ -10,16 +11,26 @@ export  const Section=styled.div`
   border-radius:10px;
   width:25%;
   height:auto;
+  
 `
 export  const GeneralSection=styled.div`
   display:flex;
   width:100%;
   height:100%;
-  background-color:#0A0A0F;
   display:flex;
   justify-content:center;
   align-items:center;
+  position:relative;
 `
+export const Img=styled.img`
+  display:flex;
+  width:100%;
+  height:100%;
+  filter:blur(8px);
+  position:absolute;
+  
+`
+
 export const Title=styled.div`
   display:flex;
   flex-direction:column;
@@ -60,7 +71,7 @@ export const LogIn=styled.div`
 export const Email=styled.div`
   display:flex;
   flex-direction:column;
-
+  margin-bottom:14px;
 `
 export const Label=styled.label`
   color:#fff;
@@ -73,6 +84,7 @@ export const Input=styled.input`
   padding:5px 12px 5px 35px;
   background-color:#161724;
   border:1px solid #1D1F27;
+  color:#fff;
   border-radius:7px;
   width:100%;
   &:hover{
@@ -94,4 +106,76 @@ export const Icon=styled(FaEnvelope)`
   position:absolute;
   color:#fff;
   left:2%;
+`
+export const LockIcon=styled(FaLock)`
+  position:absolute;
+  color:#fff;
+  left:2%;
+`
+export const Button=styled.button`
+  padding:7px 14px 7px 14px;
+  font-family:sans-serif;
+  font-size:13px;
+  color:#fff;
+  font-weight:bold;
+  background-color:rgb(75, 209, 226);
+  border-radius:10px;
+  border:none;
+  &:hover{
+    background-color:rgba(43, 162, 177, 1);
+  }
+  transition:background-color 0.3s;
+`
+export const Pargfh=styled.p`
+  color:#71717A;
+  font-family:sans-serif;
+  font-size:12px;
+`
+export const OrSection=styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`
+export const Buttons=styled.div`
+  display:flex;
+  flex-direction:row;
+  column-gap:5px;
+`
+
+export const Google=styled.button`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  border-radius:10px;
+  flex-grow:0.5;
+  background-color:#15131C;
+  color:#71717A;
+  font-family:sans-serif;
+  border:none;
+  margin-bottom:21px;
+  padding:10px 15px 10px 15px;
+`
+export const FaceBook=styled(Google)``
+export const InnerDiv=styled.div`
+  display:flex;
+  flex-direction:row;
+  column-gap:10px;
+  align-items:center;
+`
+export const Footer=styled.div`
+  display:flex;
+  flex-direction:row;
+  font-family:sans-serif;
+  justify-content:center;
+  align-items:center;
+`
+export const P1=styled.p`
+  color:#71717A;
+  font-size:13px;
+
+`
+export const P2=styled.p`
+  color:#CA4ADB;
+  font-size:13px;
+  cursor:pointer;
 `
